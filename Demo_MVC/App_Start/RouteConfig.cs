3642f60f8cbfb8092d6b126984daaff10af10ece
Tamilarasi <Tamilarasi@DESKTOP-7VRA19A>
2020-02-12 14:46:38 +0530

    Add project files.

---
+++ Demo_MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Demo_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+            name: "Login",
+            url: "Login/Index{id}",
+            defaults: new { controller = "Login", action = "Index" }
+        );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
